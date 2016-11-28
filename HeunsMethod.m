3231955bb9a57cd0e7b58e54a8dfeca1471bfff4
Adebajo <bankiadebajo@hotmail.com>
2016-11-27 20:56:42 -0500

    Complete and test RungeKutta2 for second order equations

--- HeunsMethod.m
+++ HeunsMethod.m
@@ -1,8 +1,12 @@
-function [ yInitial ] = HeunsMethod( yInitial,step,lowerBound,upperBound,func  )
+function [ y ] = HeunsMethod( yInitial,step,lowerBound,upperBound,func  )
 % Solve a differential equation using Heuns Method
+    y = zeros(size(lowerBound:step:(upperBound-step),2),1);
+    yIndex = 1;
     for t=lowerBound:step:(upperBound-step)
         p = yInitial + step*func(t,yInitial);
         yInitial = yInitial + 0.5*step*(func(t,yInitial)+func(t+step,p));
+        y(yIndex,1) = yInitial;
+        yIndex = yIndex + 1;
     end
 end
 
---
+++ RungeKutta2.m
@@ -0,0 +1,19 @@
+function [ y ] = RungeKutta2( yInitial,step,lowerBound,upperBound,func )
+% Solves a differential equation using 2nd order Runge Kutta
+    y = zeros(size(lowerBound:step:(upperBound-step),2),1);
+    yIndex = 1;
+    for t=lowerBound:step:(upperBound-step)
+        k1 = step*func(t,yInitial);
+        if(t == (upperBound-2*step))
+            k2 = step*func(t+step,yInitial+k1);
+        else
+            k2 = step*func(t,yInitial+k1);
+        end
+        
+        yInitial = yInitial + 0.5*(k1 + k2);
+        y(yIndex,1) = yInitial;
+        yIndex = yIndex + 1;
+        
+        %int32((t/step)+1)
+    end
+end
